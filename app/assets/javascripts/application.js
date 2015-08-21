99b1b50499c3069145b0ccce32f9e0e95fa3a385
adamzuckerberg <adam@adamzuckerberg.com>
2015-08-21 10:51:35 -0700

    Fix logo and remove FB login

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -46,7 +46,7 @@ $(document).ready(function() {
     $("#logo3-falling-f").css("position", "absolute" );
     $("#logo3-falling-f").css("z-index", "-9999" );
     $("#logo3-falling-f").css("font-size", "1em" );
-    $("#logo3-falling-f").css("margin", "17px 0px 0px 4px" );
+    $("#logo3-falling-f").css("margin", "37px 0px 0px 4px" );
     $("#logo3-falling-f").css("-webkit-transform", "rotate(55deg)" );
     $("#logo3-falling-f").css("-moz-transform", "rotate(55deg)" );
     $("#logo3-falling-f").css("-ms-transform", "rotate(55deg)" );
