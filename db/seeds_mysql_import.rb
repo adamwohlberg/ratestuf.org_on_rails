5464750868f3352e8ea7e49e07ba0f70a078e463
adamzuckerberg <adam@adamzuckerberg.com>
2015-08-29 14:52:44 -0700

    Commit after installing the seed dump gem and seed dumping the data that was originally imported from an old MySQL db from PHPAdmin from the old, obsolete piece of foobar PHPApplication

---
+++ db/seeds_mysql_import.rb
@@ -0,0 +1,19 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+unless Rails.env.production?
+  connection = ActiveRecord::Base.connection
+  sql = File.read('db/import.sql')
+  statements = sql.split(/;$/)
+  statements.pop
+
+  ActiveRecord::Base.transaction do
+    statements.each do |statement|
+      connection.execute(statement)
+    end
+  end
+end
