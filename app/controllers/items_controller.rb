72463accaa64839b190ed6affbe9ffae03644b68
adamzuckerberg <adam@adamzuckerberg.com>
2015-09-02 11:44:23 -0700

    If search is for a category, the items in the category are displayed and the validation code to check if the items exists is skipped since for any category search, the items in that category already exist a priori

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,72 @@
 class ItemsController < ApplicationController
 
-	def create
-    # @items = params[:search].present? ? Rating.search(params[:search]) : []
-    # 	@items.each do |item|
-    # 		next if Item.find_by_name(item.strip).exists?
-    # 		if !user_signed_in? 
-    # 			 redirect_to :root
-    #   		 flash[:alert] = "One or more of your item(s) is not currently in our database. Please sign in to continue."
-    # 		end
-    # 		Item.create!()
+	layout 'application'
 
-    # 	end
-	end
+  def index
+    @items = params[:search].present? ? Item.search(params[:search]) : []
+    if params[:search].blank?
+      flash.now[:alert] = "Please search for something (e.g. 'uber', 'uber vs. lyft', or 'airlines')."
+    # search for a single item that does not exist in the db
+    elsif @items.count == 0
+       flash.now[:alert] = "This item does not exist in our database yet.  You must log in or try another search term."
+    #search for a category with multiple items already in the db
+    elsif Category.where("name LIKE ?", "%#{params[:search]}%").exists?
+       return
+    #search for multiple items, one or more of which does not exist in db
+    elsif Item.filter_search_query(params[:search]).count != Item.search(params[:search]).count
+       flash.now[:alert] = "One or more of your items is new to our system.  You must log in to continue this search."
+    end
+  end
+
+  def create
+    if !params[:search].present? 
+      flash[:alert] = "Please search for something (e.g. 'uber', 'uber vs. lyft', or 'airlines')."
+    else
+
+    # @items = params[:search].present? ? Rating.search(params[:search]) : []
+    #   @items.each do |item|
+    #     next if Item.find_by_name(item.strip).exists?
+    #     if !user_signed_in? 
+    #        redirect_to :root
+    #        flash[:alert] = "One or more of your item(s) is not currently in our database. Please sign in to continue."
+    #     end
+    #     Item.create!()
+
+    #   end
+  # end
+
+
+
+
+
+
+      flash[:alert] = ""
+      add_new_items    
+		  @items = params[:search].present? ? Item.search(params[:search]) : []
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private 
+
+  def add_new_items
+    @items = Item.search(params[:search])
+    @items.each do |item|
+      next if Item.find_by_name(item.strip).exists?
+      if !user_signed_in? 
+         redirect_to :root
+         flash[:alert] = "One or more of your item(s) is not currently in our database. Please sign in to continue."
+      else
+        Item.create!(name: item.name)
+      end
+    end
+  end
+
+
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,29 @@ class Item < ActiveRecord::Base
 	has_many :categories, through: :items_categories
 	has_many :ratings
 
+	def self.filter_search_query(search_term, result=[])
+		# order of terms with periods should be listed prior to the same term with no period to avoid duplicate results e.g. ['uber','. uber']
+		terms_that_mean_versus = [' versus ', ' versus', ' versus', 'versus',' vs.', 'vs.' ,' vs',' vs ']
+	  search_term = search_term.strip
+		terms_that_mean_versus.each do |versus|
+			if search_term.include?(versus)
+				words = search_term.split(versus)
+				words.each do |word|
+					result << (word.strip)
+				end
+				break
+			end
+		end
+		return result.present? ? result.flatten.uniq : [search_term]
+	end
+
+	def self.search(search_term, result=[])
+		params = filter_search_query(search_term)
+		params.each do |query|
+		result << Item.find_by_sql(["SELECT (AVG(x_rating)*100) AS xRating, (AVG(y_rating)*100) AS yRating, COUNT(x_rating) AS votes, items.name, items.id, items.url FROM `items` JOIN items_categories ON items.id = items_categories.item_id JOIN `categories` ON categories.id = items_categories.category_id JOIN `ratings` ON ratings.item_id = items.id WHERE items.name LIKE concat('%', ?, '%') OR categories.name like concat('%', ?, '%') GROUP BY items.name LIMIT 10", query, query])
+	  end
+	  terms = result.flatten
+	  return terms
+	end
+
 end
