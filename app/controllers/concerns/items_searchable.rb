310c792028b98fd15ddfb4ba6bb709dc8ca08618
adamzuckerberg <adam@adamzuckerberg.com>
2015-09-02 15:03:45 -0700

    Refactor the items controller index action

--- app/controllers/concerns/items_searchable.rb
+++ app/controllers/concerns/items_searchable.rb
@@ -1,25 +1,47 @@
 module ItemsSearchable
 extend ActiveSupport::Concern
+	PLEASE_SEARCH =  "Please search for something "\
+		"(e.g. 'uber', 'uber vs. lyft', or 'airlines')."
+	NEW_ITEM = "One or more of your items is new to our system.  "\
+		"You must log in to complete this search."
+	SIGN_IN = "One or more of your item(s) is not currently in our database. "\
+		"Please sign in to continue."
 
 	def search(search_string)
-    @items = search_string.present? ? Item.search(search_string) : []
-    #search without a search term
-    if search_string.blank?
-      return "Please search for something (e.g. 'uber', 'uber vs. lyft', or 'airlines')."
-    # search for a single item that does not exist in the db
-    elsif @items.count == 0
-      if user_signed_in?
-        # add the item to the db
-      elsif !user_signed_in?
-       return "One or more of your item(s) is not currently in our database. Please sign in to continue."
-      end
-    #search for a category with multiple items already in the db
-    elsif Category.where("name LIKE ?", "%#{search_string}%").exists?
-       return
-    #search for multiple items, one or more of which does not exist in db
-    elsif Item.filter_search_query(search_string).count != Item.search(search_string).count
-       return "One or more of your items is new to our system.  You must log in to complete this search."
-    end		
+		@search_string = search_string
+		set_items
+		case 
+		when @search_string.blank? then PLEASE_SEARCH 
+		when should_add_item?
+	    # add the item to the db		
+		when should_sign_in? then SIGN_IN
+		when category_exists?
+		when new_item? then NEW_ITEM
+		end	
+	end
+
+	private
+
+	def set_items
+		return @items = [] if @search_string.blank?
+		@items = Item.search(@search_string) 
+	end
+
+	def category_exists?
+		Category.where("name LIKE ?", "%#{@search_string}%").exists?
+	end
+
+	def new_item?
+		Item.filter_search_query(@search_string).count != 
+		Item.search(@search_string).count	
+	end
+
+	def should_add_item?
+		@items.count.zero? && user_signed_in?
+	end
+
+	def should_sign_in?
+		@items.count.zero? && !user_signed_in?
 	end
 
 end
