2cfe8068e60eac12622ab0637a918ddd6b497673
adamzuckerberg <adam@adamzuckerberg.com>
2015-09-02 14:12:28 -0700

    Adds test coverage to 100% of the items controller, index action

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -15,5 +15,31 @@ class ItemsControllerTest < ActionController::TestCase
     assert_not_equal "FOOBAR One or more FOOBAR of your item(s) is not currently in our database. Please sign in to continue FOOBAR.", flash[:alert] 
   end
 
+  test "the index with search params which is a category" do
+  	category = Category.create!(name: 'airlines')
+  	assert Category.where("name LIKE ?", "%airlines%").exists?
+  	item = Item.create!(name: 'delta')
+   	Rating.create!(item_id: item.id, x_rating: 0.5, y_rating: 0.5)
+  	ItemsCategory.create!(item_id: item.id, category_id: category.id)
+  	assert_not_equal 0,Item.search('airlines').count
+    get :index, search: 'airlines'
+    assert_response :ok
+  end
+
+  test "the index with search params for multiple items" do
+  	category = Category.create!(name: 'airlines')
+  	assert Category.where("name LIKE ?", "%airlines%").exists?
+  	item = Item.create!(name: 'delta')
+   	Rating.create!(item_id: item.id, x_rating: 0.5, y_rating: 0.5)
+  	ItemsCategory.create!(item_id: item.id, category_id: category.id)
+  	assert_not_equal 0,Item.search('airlines').count
+    get :index, search: 'delta vs foobar'
+    assert_response :ok
+  end
+
 end
  
+
+
+
+
