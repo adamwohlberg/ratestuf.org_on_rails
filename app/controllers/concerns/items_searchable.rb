d9b43f62a7cdaf010b18f6639c09197839590139
adamzuckerberg <adam@adamzuckerberg.com>
2015-09-03 20:09:18 -0700

    Mucho sausage

--- app/controllers/concerns/items_searchable.rb
+++ app/controllers/concerns/items_searchable.rb
@@ -2,24 +2,26 @@ module ItemsSearchable
 extend ActiveSupport::Concern
 	PLEASE_SEARCH =  "Please search for something "\
 		"(e.g. 'uber', 'uber vs. lyft', or 'airlines')."
-	NEW_ITEM = "One or more of your items is new to our system.  "\
+	NEW_ITEM_PLEASE_SIGN_IN = "One or more of your items is new to our system.  "\
 		"You must log in to complete this search."
 	SIGN_IN = "One or more of your item(s) is not currently in our database. "\
 		"Please sign in to continue."
-	ITEM_ADDED = "Your new item was added to our database"
+	ITEM_ADDED = "Your new item was added to our database."
+	TOO_MANY_ITEMS = "You have created too many items."
 
 	def search(search_string)
 		@search_string = search_string
 		set_items
 		case 
 		when @search_string.blank? then PLEASE_SEARCH 
-		when should_sign_in? then SIGN_IN
+		when user_has_created_too_many_items? then TOO_MANY_ITEMS
 		when should_add_item?
 			create_item_and_associated_objects
-			redirect_to :root
 			ITEM_ADDED
+			redirect_to :root
 		when category_exists?
-		when new_item? then NEW_ITEM
+		when new_item_but_not_signed_in? then NEW_ITEM_PLEASE_SIGN_IN
+		when should_sign_in? then SIGN_IN
 		end	
 	end
 
@@ -34,13 +36,12 @@ extend ActiveSupport::Concern
 		Category.where("name LIKE ?", "%#{@search_string}%").exists?
 	end
 
-	def new_item?
-		Item.filter_search_query(@search_string).count != 
-		Item.search(@search_string).count	
+	def new_item_but_not_signed_in?
+		!user_signed_in? && (Item.filter_search_query(@search_string).count != Item.search(@search_string).count)	
 	end
 
 	def should_add_item?
-		@items.count.zero? && user_signed_in?
+		(@items.count.zero? && user_signed_in?) || (user_signed_in? && (Item.filter_search_query(@search_string).count != Item.search(@search_string).count))
 	end
 
 	def should_sign_in?
@@ -49,6 +50,10 @@ extend ActiveSupport::Concern
 
 	def create_item_and_associated_objects
 	  ItemFactory.create!(@search_string)
+	end
+
+	def user_has_created_too_many_items?
+		current_user && (Item.where(user_id: current_user.id).count > 100)
 	end
 
 end
---
+++ db/migrate/20150904024304_update_index.rb
@@ -0,0 +1,5 @@
+class UpdateIndex < ActiveRecord::Migration
+  def change
+  	rename_index :items_categories, 'item_subcategory_index', 'item_category_index'
+  end
+end
