ccf3fc559f0f4205430ed41b54b602daf9a3586c
Kalpna <kalpna@kalpna.com>
2015-09-21 01:42:51 +0530

    the scope changes task

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -7,7 +7,7 @@ class RatingsController < ApplicationController
       @items.each do |item|
         #TODO add item already rated by this user then update instead of adding a new rating
         if already_rated_n_times?(item, 2)
-          @last_rating = Rating.where(user_id: current_user.id, item_id: item['id']).last
+          @last_rating = Rating.last_rating_of_user(current_user.id, item)
           @rating = @last_rating.update_attributes(user_id: current_user.id, item_id: item['id'], x_rating: item['x_rating'], y_rating: item['y_rating'])
           session[:message] = 'Congratulations! Your rating(s) were updated.'
         else
@@ -16,17 +16,15 @@ class RatingsController < ApplicationController
         @item_ids << item['id']
       end
       session[:message] ||= 'Congratulations! Your rating(s) were saved.'
-    end 
-    @items = Item.where(id: @item_ids)
+    end
+    @ratings = Rating.generate_format(Rating.item_ratings(@item_ids))
     respond_to do |format|
-        format.json { render :json => @items.to_json(:include => :ratings) }
+      format.json { render :json => @ratings.to_json }
     end
   end
 
-  private 
-
+  private
   def already_rated_n_times?(item, number)
-    true if Rating.where(user_id: current_user.id, item_id: item['id']).count > number
+    Rating.fetch_rating(current_user.id, item).count > number
   end
-
 end
