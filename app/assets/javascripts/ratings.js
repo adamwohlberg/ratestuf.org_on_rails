e06032e2929227237b594590004c67c1f3aa7e52
kalpna <pinak.thakkar90@gmail.com>
2015-09-21 14:18:27 +0530

    Improvements and ruby upgraded

--- app/assets/javascripts/ratings.js
+++ app/assets/javascripts/ratings.js
@@ -24,6 +24,7 @@ $(document).ready(function() {
 
 	  x_rating = (Math.round((positionFromLeft / containerWidth) * 100 )/ 100);
 	  y_rating = (Math.round((1-(positionFromTop / containerHeight))* 100 )/ 100);
+		// alert("containerWidth : " + containerWidth + "containerHeight" + containerHeight + "x_r" + x_rating + "x_y" + y_rating + "posl" + positionFromLeft + "post" + positionFromTop);
 
   	data.items.push({"name": name, "id": id, "x_rating":x_rating, "y_rating":y_rating});
 		});
@@ -36,7 +37,10 @@ $(document).ready(function() {
 	  success: function(json) {
 	  	console.log(json);
 			$.each(json, function(i, item) {
-			  $('#'+ json[i].id + " p").text(json[i].count);
+				position_left = json[i].x_rating * containerWidth;
+				position_top  = (containerHeight - (containerHeight * json[i].y_rating));
+			  $('#'+ json[i].id + " p[class=ratings]").text(json[i].count-1 + ' ratings');
+				$('#'+ json[i].id).css("left", position_left).css('top', position_top);
 			});
 	  },
 	  error: function(json) {
