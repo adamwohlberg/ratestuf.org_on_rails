ddddc6dd7f92bd2ba7677b58baec7117fdcfe79d
adamzuckerberg <adam@adamzuckerberg.com>
2015-09-24 16:38:39 -0400

    working on it

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,10 @@ class ItemsController < ApplicationController
   def index
     flash.now[:alert] = search(params[:search])
     flash.now[:notice] = session[:message] if session[:message].present?
+    respond_to do |f|
+    	format.html
+    	format.json { render :json => @items.to_json }
+    end
   end
 
 end
