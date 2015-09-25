ae350eaddcfc076855446ecf82d702c65ccbd255
adamzuckerberg <adam@adamzuckerberg.com>
2015-09-25 11:25:10 -0400

    Ajax ball creation

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,11 +1,11 @@
+function OnloadFunction ()
+{
 
 $(document).foundation();
 
-$(document).ready(function() {
   setTimeout(function() {
 	$("#starCounter").css("display", "block");
 	}, 100);	
-});
 
 function tagLineLetterFallsOnLoad() {
  setTimeout(function() {
@@ -23,13 +23,10 @@ function tagLineLetterFallsOnLoad() {
  },1900);
 }
 
-$(document).ready(function() {
 	$(".draggable").dblclick(function(){
 	    $(this).fadeOut(100);
 		});
-});
 
-$(document).ready(function() {
 	$(".draggable").mousemove(function(){
 	  var counter = ((((($(this).position().left)/($('#containmentWrapper').width()-($(this).width()/2))*100)/20)+0.4).toFixed(1));
 	    if (counter == 0.4) {
@@ -39,9 +36,7 @@ $(document).ready(function() {
 	    }
 	    $("#starCounter").html(counter);
 	});
-});
 
-$(document).ready(function() {
 $(".draggable").mousemove(function(){
 	  $(".star").removeClass('starhover');
 
@@ -78,27 +73,17 @@ $(".draggable").mousemove(function(){
 	    $("#oneDollar").addClass('dollarhover');
 	  }
 	});
-});
 
-
-
-// display number of ratings
-$(document).ready(function() {
   $(".draggable").hover(function() {
       $(this).find(".speechBubble").fadeIn(1000).delay(50).fadeOut(2500);
       $(this).find(".ratings").fadeIn(1000).delay(50).fadeOut(2500);
       return;
   });
-});
 
-$(document).ready(function() {
 	$(function() {
 	  $(".draggable").draggable({ containment: "#containmentWrapper" });
 	  });
-});
 
-
-$(document).ready(function() {
 	$(".draggable").each(function(){
 
 	  xPosition = (Math.round(($(this).position().left / ($(this).parent().width())) * 100));
@@ -127,9 +112,7 @@ $(document).ready(function() {
 	        $(this).addClass('bestValue'); 
 	  }
 	}); 
-}); 
 
-$(document).ready(function() {
 	$(".draggable").mouseover(function(){
 	  xPosition = (Math.round(($(this).position().left / ($(this).parent().width())) * 100));
 	  yPosition = (100-(Math.round(($(this).position().top / ($(this).parent().height())) * 100)));
@@ -155,6 +138,57 @@ $(document).ready(function() {
 	        $(this).addClass('bestValue'); 
 	  }
 	}); 
-}); 
 
+// *****************************************
+// CREATE RATINGS
+// *****************************************
+var data ={};
+data.items = [];
 
+	$("#rateNowButton").click(function(event){
+		event.preventDefault();
+	  if ($(this).hasClass('disabled')) {
+	  	alert('You must log in to rate stuff.')
+	    return;}
+	$("#rateNowButton").addClass('disabled');
+  $('.draggable').each(function() {
+	  name = $(this).attr('name');
+	  id = $(this).attr('id');
+	  containerHeight = ($(this).parent().height() * 0.78 );
+	  containerWidth = ($(this).parent().width() * 0.896);
+	  positionFromLeft = ($(this).position().left);
+	  positionFromTop = ($(this).position().top);
+	  x_rating = (Math.round((positionFromLeft / containerWidth) * 100 )/ 100);
+	  y_rating = (Math.round((1-(positionFromTop / containerHeight))* 100 )/ 100);
+  	data.items.push({"name": name, "id": id, "x_rating":x_rating, "y_rating":y_rating});
+		});
+
+	 $.ajax({
+	  data: JSON.stringify(data),
+	  type: "POST",
+	  url: "/ratings",
+	  contentType: "application/json",
+	  success: function(json) {
+	  	console.log(json);
+			$.each(json, function(i, item) {
+				position_left = json[i].x_rating * containerWidth;
+				position_top  = (containerHeight - (containerHeight * json[i].y_rating));
+			  $('#'+ json[i].id + " p[class=ratings]").text(json[i].count + ' ratings');
+				$('#'+ json[i].id).css("left", position_left).css('top', position_top);
+			  if (json[i].count === 3) {
+				$('p[class=notice]').text('You can only rate an item three times. Future ratings will update your last rating.');
+			  } else if (json[i].count < 3) {
+				$('p[class=notice]').text('Congratulations! Your rating(s) were saved.');
+				};
+			});
+	  },
+	  error: function(json) {
+	  	console.log('error');
+	  },
+	  dataType:'json'});
+	 data.items = [];
+	  $("#rateNowButton").removeClass('disabled');
+	});
+
+}
+$(document).ready(OnloadFunction);
