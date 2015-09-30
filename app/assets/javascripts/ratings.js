79119ab2a13e0372df744ea15d2376e3ab6bcfd4
adamzuckerberg <adam@adamzuckerberg.com>
2015-09-30 09:35:57 -0700

    Moves AJAX code to create.js.erb for ratings with javascript. Adds appropriate flash messages and logic

--- app/assets/javascripts/ratings.js
+++ app/assets/javascripts/ratings.js
@@ -1,49 +1,42 @@
-$( document ).ready(function() {
 // *****************************************
 // CREATE RATINGS
 // *****************************************
-var data ={};
-data.items = [];
+$( document ).ready(function() {
+	var data ={};
+	data.items = [];
 
 	$("#rateNowButton").click(function(event){
 		event.preventDefault();
-	  if ($(this).hasClass('disabled')) {
+	  if ($(this).hasClass('disabled')){
 	  	alert('You must log in to rate stuff.')
-	    return;}
-	$("#rateNowButton").addClass('disabled');
-  $('.draggable').each(function() {
-	  name = $(this).attr('name');
-	  id = $(this).attr('id');
-	  containerHeight = ($(this).parent().parent().height() * 0.78 );
-	  containerWidth = ($(this).parent().parent().width() * 0.896);
-	  positionFromLeft = ($(this).position().left);
-	  positionFromTop = ($(this).position().top);
-	  x_rating = (Math.round((positionFromLeft / containerWidth) * 100 )/ 100);
-	  y_rating = (Math.round((1-(positionFromTop / containerHeight))* 100 )/ 100);
-  	data.items.push({"name": name, "id": id, "x_rating":x_rating, "y_rating":y_rating});
+	    return;
+	  }
+
+		$("#rateNowButton").addClass('disabled');
+
+	  $('.draggable').each(function() {
+		  name = $(this).attr('name');
+		  id = $(this).attr('id');
+		  containerHeight = ($(this).parent().parent().height() * 0.78 );
+		  containerWidth = ($(this).parent().parent().width() * 0.896);
+		  positionFromLeft = ($(this).position().left);
+		  positionFromTop = ($(this).position().top);
+		  x_rating = (Math.round((positionFromLeft / containerWidth) * 100 )/ 100);
+		  y_rating = (Math.round((1-(positionFromTop / containerHeight))* 100 )/ 100);
+	  	data.items.push({"name": name, "id": id, "x_rating":x_rating, "y_rating":y_rating});
 		});
 
-	 $.ajax({
-	  data: JSON.stringify(data),
-	  type: "POST",
-	  url: "/ratings",
-	  contentType: "application/json",
-	  success: function(json) {
-	  	$('.notice').hide();
-			$('.alert').hide();
-			$.each(json, function(i, item) {
-				position_left = json[i].x_rating * containerWidth;
-				position_top  = (containerHeight - (containerHeight * json[i].y_rating));
-			  $('#'+ json[i].id + " p[class=ratings]").text((json[i].count == 1) ? json[i].count + ' rating' : json[i].count + ' ratings' );
-				$('#'+ json[i].id).css("left", position_left).css('top', position_top);
-				$("#flash_messages").html("<p class='notice'>Congratulations! Your rating(s) were saved. Note: you can only rate an item one (1) time. Future attempts will simply update your previous rating.</p>");
-			});
-	  },
-	  error: function(json) {
-	  	console.log('error');
-	  },
-	  dataType:'json'});
-	 data.items = [];
-	  $("#rateNowButton").removeClass('disabled');
+		$.ajax({
+			data: data,
+			type: "POST",
+			url: "/ratings",
+			contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+			dataType: 'script'
+		});
+
+		data.items = [];
+		$("#rateNowButton").removeClass('disabled');
+
 	});
+
 });
--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,15 +2,17 @@ class RatingsController < ApplicationController
   
   def create
     session[:message] = nil
-    @items = params[:items]
+    @items = params['items']
     if @items.present?
       @item_ids = []
-      @items.each do |item|
+      @items.keys.each do |key|
+        item = @items[key]
         if only_has_default_rating?(item)
           update_default_rating(item)
           flash.now[:notice] = 'Congratulations! Your rating(s) were saved.'
         elsif already_rated_n_times?(item, 0)
           update_rating(item)
+          flash.now[:notice] = "You can only rate an item one (1) time. Future attempts will simply update your previous rating."
         else
           create_rating(item)
           flash.now[:notice] = 'Congratulations! Your rating(s) were saved.'
@@ -20,15 +22,15 @@ class RatingsController < ApplicationController
     end
     @ratings = Rating.generate_format(Rating.item_ratings(@item_ids))
     respond_to do |format|
-      format.json { render :json => @ratings.to_json }
+      format.js {}
     end
   end
 
   private
 
   def only_has_default_rating?(item)
-    Item.where(name: item[:name]).first.ratings.count == 1 &&
-    Item.where(name: item[:name]).first.ratings.first.default_rating == true
+    item_ratings = Item.find_by(name: item[:name]).ratings
+    item_ratings.count == 1 && item_ratings.first.default_rating
   end
 
   def already_rated_n_times?(item, number)
