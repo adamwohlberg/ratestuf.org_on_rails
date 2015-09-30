55dc962d034c8f999546b4f134b60cbe30d6b346
kalpna <pinak.thakkar90@gmail.com>
2015-09-30 15:12:51 +0530

    simplifying ajax #10 and Users need to be notified #9

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,16 +1,18 @@
 class RatingsController < ApplicationController
-  
+
   def create
     session[:message] = nil
-    @items = params[:items]
+    @items = params["items"]
     if @items.present?
       @item_ids = []
-      @items.each do |item|
+      @items.keys.each do |key|
+        item = @items[key]
         if only_has_default_rating?(item)
           update_default_rating(item)
           flash.now[:notice] = 'Congratulations! Your rating(s) were saved.'
         elsif already_rated_n_times?(item, 0)
           update_rating(item)
+          flash.now[:notice] = "You can only rate an item one (1) time. Future attempts will simply update your previous rating."
         else
           create_rating(item)
           flash.now[:notice] = 'Congratulations! Your rating(s) were saved.'
@@ -20,15 +22,15 @@ class RatingsController < ApplicationController
     end
     @ratings = Rating.generate_format(Rating.item_ratings(@item_ids))
     respond_to do |format|
-      format.json { render :json => @ratings.to_json }
+      format.js {}
     end
   end
 
   private
 
   def only_has_default_rating?(item)
-    Item.where(name: item[:name]).first.ratings.count == 1 &&
-    Item.where(name: item[:name]).first.ratings.first.default_rating == true
+    item_ratings = Item.find_by(name: item[:name]).ratings
+    item_ratings.count == 1 && item_ratings.first.default_rating
   end
 
   def already_rated_n_times?(item, number)
@@ -36,7 +38,7 @@ class RatingsController < ApplicationController
   end
 
   def update_default_rating(item)
-    @default_rating = Rating.where(item_id: item['id']).first
+    @default_rating = Rating.find_by(item_id: item['id'])
     @default_rating.update_attributes(user_id: current_user.id, x_rating: item['x_rating'], y_rating: item['y_rating'], default_rating: false)
   end
 
@@ -44,9 +46,8 @@ class RatingsController < ApplicationController
     @rating = Rating.create!(user_id: current_user.id, item_id: item['id'], x_rating: item['x_rating'], y_rating: item['y_rating'], default_rating: false)
   end
 
-  def update_rating(item) 
+  def update_rating(item)
     @last_rating = Rating.last_rating_of_user(current_user.id, item)
     @last_rating.update_attributes(user_id: current_user.id, item_id: item['id'], x_rating: item['x_rating'], y_rating: item['y_rating'])
   end
-
 end
